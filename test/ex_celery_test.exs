cd7f5b7225b18bcce4f4c439a23e651a72934151
Rob Golding <rob@robgolding.com>
2016-11-01 22:17:58 +0000

    Move `broker_url`, `args` and `kwargs` to optional keyword list `opts`
    Also moves the `exchange` to an option that's provided when the process
    is started, rather than every time a task is delayed.

--- test/ex_celery_test.exs
+++ test/ex_celery_test.exs
@@ -1,10 +1,8 @@
 defmodule ExCeleryTest do
   use ExUnit.Case
 
-  @broker_url "amqp://guest:guest@localhost"
-
   setup do
-    {:ok, amqp} = AMQP.Connection.open(@broker_url)
+    {:ok, amqp} = AMQP.Connection.open
     {:ok, chan} = AMQP.Channel.open(amqp)
 
     AMQP.Queue.declare(chan, "celery")
@@ -13,7 +11,7 @@ defmodule ExCeleryTest do
       routing_key: "#",
     ])
 
-    {:ok, celery} = ExCelery.start_link(@broker_url)
+    {:ok, celery} = ExCelery.start_link
     {:ok, %{chan: chan, celery: celery}}
   end
 
@@ -23,7 +21,7 @@ defmodule ExCeleryTest do
   end
 
   test "apply_async empty", %{chan: chan, celery: celery} do
-    ExCelery.apply_async(celery, "test.task", [], %{})
+    ExCelery.apply_async(celery, "test.task")
     :timer.sleep(100)
     {payload, meta} = get_message(chan)
     assert payload["task"] == "test.task"
@@ -34,7 +32,10 @@ defmodule ExCeleryTest do
   end
 
   test "apply_async with args", %{chan: chan, celery: celery} do
-    ExCelery.apply_async(celery, "test.task", ["arg_1"], %{"kwarg_1": "value_1"})
+    ExCelery.apply_async(celery, "test.task", [
+      args: ["arg_1"],
+      kwargs: %{"kwarg_1": "value_1"},
+    ])
     :timer.sleep(100)
     {payload, meta} = get_message(chan)
     assert payload["task"] == "test.task"
@@ -46,7 +47,7 @@ defmodule ExCeleryTest do
   end
 
   test "apply_async with opts", %{chan: chan, celery: celery} do
-    ExCelery.apply_async(celery, "test.task", [], %{}, [
+    ExCelery.apply_async(celery, "test.task", [
       routing_key: "test",
       content_encoding: "ascii",
       persistent: false
